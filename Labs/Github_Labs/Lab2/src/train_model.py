36d4fe6e2472f2f9dc3c0033c737c10ac5f9928d
Isha0501 <ishanevatia@gmail.com>
2026-01-31 00:11:41 -0500

    Github Lab2: Rename model keyword back to forest

--- Labs/Github_Labs/Lab2/src/train_model.py
+++ Labs/Github_Labs/Lab2/src/train_model.py
@@ -65,15 +65,15 @@ if __name__ == '__main__':
         mlflow.log_params(params)
             
         # Implementing LGBM instead of Random Forest
-        lgbm_model = LGBMClassifier(
+        forest = LGBMClassifier(
             random_state=0,
             n_estimators=100,  
             learning_rate=0.1,
             verbose=-1  
         )
 
-        lgbm_model.fit(X, y)
-        y_predict = lgbm_model.predict(X)
+        forest.fit(X, y)
+        y_predict = forest.predict(X)
         mlflow.log_metrics({'Accuracy': accuracy_score(y, y_predict),
                             'F1 Score': f1_score(y, y_predict)})
         
@@ -84,4 +84,4 @@ if __name__ == '__main__':
         # After retraining the model
         model_version = f'model_{timestamp}'  # Use a timestamp as the version
         model_filename = f'{model_version}_lgbm_model.joblib'  # Changed filename
-        dump(lgbm_model, model_filename)
+        dump(forest, model_filename)
